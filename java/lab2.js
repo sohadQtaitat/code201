e3624c419676e9d59fd001a4ef052bf47e3f77b5
sohadQtaitat <qtaitat3@gmail.com>
2019-10-07 14:48:19 +0300

    this is lab2 in code 201

---
+++ java/lab2.js
@@ -0,0 +1,69 @@
+'use strict';
+
+
+
+var love = prompt('Do you love coding ?');
+
+        // var convertedLove = love.toLowerCase();
+
+        // if i want to use the upper case the value of love in is statmemnt must be captel latter
+        love = love.toLowerCase();
+
+
+
+switch(love){
+case'yes': 
+case 'y':
+alert('of course')
+break;
+default:
+    alert('what is up ???')
+}
+
+
+      
+
+
+var fruits =['apple','banana','orange'];
+
+fruits[0];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        if (love == 'yes') {//YES,yEs,YEs,
+            alert('of course you do ');
+        }
+        else if (love == 'y') {
+            alert('of course you do ');
+        }
+
+
+        else if (love == 'no') {
+            alert('why you can\'t  ');
+        }
+        else if (love == 'n') {
+            alert('of course you do ');
+        }
+
+
+        else {
+
+            alert('how odd');
+        }
+
+
+
+
+        // alert('Hi');
+        //alert(love)
